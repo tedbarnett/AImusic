8abaa581e2addd0e4dfcfcab1864385efe6b9259
Ted Barnett <mail@tedbarnett.com>
2023-04-30 16:36:30 -0400

    Added Genesis-like song to melody.txt

--- play_AI_music.py
+++ play_AI_music.py
@@ -3,7 +3,7 @@ from asyncio import wait
 from scamp import Session
 
 s = Session(tempo=60)
-clar = s.new_part("clarinet", 0)
+clar = s.new_part("violin", 0)
 
 def load_note_variables():
     with open("notes.txt", "r") as file:
@@ -17,21 +17,31 @@ def load_melody_from_file(filename):
     melody = []
     with open(filename, "r") as file:
         for line in file.readlines():
-            note, duration = line.strip().split(",")
-            pitch_string = note.strip()
-            duration_value = eval(duration.strip())
-            pitch_value = globals().get(pitch_string, pitch_string)
-            melody.append((pitch_value, duration_value))
+            try:
+                note, duration = line.strip().split(",")
+                if "/" in duration:
+                    numerator, denominator = map(int, duration.split("/"))
+                    duration = numerator / denominator
+                    duration = float(duration)
+                pitch_string = note.strip()
+                duration_value = eval(duration.strip())
+                pitch_value = globals().get(pitch_string, pitch_string)
+                melody.append((pitch_value, duration_value))
+            except ValueError:
+                print(f"{line.strip()}")
     return melody
 
-melody = load_melody_from_file("melody.txt")
-print(f"melody = {melody}")
 
+
+def get_pitch_string(pitch_value):
+    pitch_map = {value: key for key, value in globals().items() if isinstance(value, float)}
+    return pitch_map.get(pitch_value, f"Unknown pitch value {pitch_value}")
 
 
 for pitch, duration in melody:
     try:
-        print(f"Playing {pitch} for {duration} beats")
+        pitch_note_name = get_pitch_string(str(pitch))
+        print(f"Playing {pitch_note_name} for {duration} beats")
         volume = 0.7
         if pitch == 0:
             wait(duration)
@@ -42,3 +52,5 @@ for pitch, duration in melody:
         print("Skipping invalid note:", e)
 
 print("Melody completed!")
+melody = load_melody_from_file("melody.txt")
+print(f"melody = {melody}")
